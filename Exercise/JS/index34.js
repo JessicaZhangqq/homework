09fc23dbc4dc0f2d6bfc1cce7f44de8796b8cdb8
張倩倩 <zhangqianqian@chousensens-MacBook-Air.local>
2018-04-12 22:18:47 -0500

    added ex 34, ex35 to project

---
+++ Exercise/JS/index34.js
@@ -0,0 +1,8 @@
+var income, revenue, taxes;
+income = 1000;
+revenue = 600;
+taxes = 500;
+console.log('Income Obejctive:true or fasle:', income>=800);
+console.log('Taxes Objective: true or false:', taxes<400);
+console.log("Bonus Objective:true or false:", revenue==600);
+
---
+++ Exercise/JS/index35.js
@@ -0,0 +1,3 @@
+var clientId = 143245;
+var inputClientId = '143245';
+console.log('if the input client id is the same with client id:',clientId == inputClientId);
