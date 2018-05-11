cc5d094d3be5652f0e74e747bb4d57e0c16be299
張倩倩 <zhangqianqian@chousensens-MacBook-Air.local>
2018-05-10 22:41:05 -0500

    modified index142.js

--- Exercise/JS/index142.js
+++ Exercise/JS/index142.js
@@ -1,5 +1,16 @@
     let firstPerson,secondPerson;
-    const newArray=['Jessica','Zhang','2475 Waverley St','30'];
-    firstPerson=newArray[0];
-    secondPerson=newArray[1];
-    
+     firstPerson=['Jessica','Zhang','2475 Waverley St','30'];
+    secondPerson=['Nicole','Guan','2475 Waverley St','31'];
+    const string1=firstPerson.reduce(function(result,item){
+        return result+item;
+    });
+    const string2=secondPerson.reduce(function(result,item){
+       return result+item;
+    });
+    if(string1==string2){
+        console.log('They are the same person.')
+    }else{
+        console.log('They are not the same person.')
+    }
+    console.log(string1,string2);
+    //Don't understand what Nico wants to do
--- Exercise/JS/test.js
+++ Exercise/JS/test.js
@@ -1,4 +1,16 @@
-var num = '15';
-var number=String.prototype.replace(num, /5/, '23333')
-console.log(number,'hi');
-console.log('hi');
+const firstPerson = ['Harpreet', 'Dhillon', 'Winnipeg', 31];
+const secondPerson = ['Harpreet', 'Dhillon', 'Sydney', 31];
+const firstString = firstPerson.reduce(function(acc, value){
+    return acc + value;
+});
+const secondString = secondPerson.reduce(function(acc, value){
+    return acc + value;
+});
+// console.log(firstString);
+// console.log(secondString);
+if(firstString === secondString){
+    console.log(`They are same person`);
+}
+else{
+    console.log('They aren\'t same person');
+}
